4b4a5cb71f47d72479900aed761f5e14023bdbca
Oyebode <adetunji.oyebode6@gmail.com>
2022-12-01 00:11:00 +0100

    Task 6

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 8):
-    for j in range(i + 1, 10):
-        print("{}{}, ".format(i, j), end='')
-        print("{}".format(89))
+for i in range(0, 99):
+    print("{:02d}, ".format(i), end='')
+print("{}".format(99))
