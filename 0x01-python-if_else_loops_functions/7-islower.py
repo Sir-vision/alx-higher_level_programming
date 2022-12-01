095ea05e35032ccabf397cc9ec3841763dd60943
Oyebode <adetunji.oyebode6@gmail.com>
2022-12-01 01:02:42 +0100

    Task 8

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if chr(ord(c) >= '97') and chr(ord(c) <= '122')
-
-
-return True
-else:
-    return False
+    if (ord(c) >= '97') and (ord(c) <= '122'):
+        return True
+    else:
+        return False
