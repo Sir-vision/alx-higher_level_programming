c13dd5a8418ba90bd394f377d56b30e355627691
Oyebode <adetunji.oyebode6@gmail.com>
2022-12-04 18:01:19 +0100

    update task 6

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    import hidden_4.pyc
+    import hidden_4
     name_list = dir(hidden_4.pyc)
     for name in name_list:
         if name[0:2] != "__":
