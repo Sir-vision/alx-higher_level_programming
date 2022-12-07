df2de343e4b4316556105c5924b72201a4a7b903
Oyebode <adetunji.oyebode6@gmail.com>
2022-12-07 22:21:16 +0100

    task5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string_list = list(my_string)
-    index_count = 0
-    for index in my_string_list:
-        if index == 'c' or index == 'C':
-            my_string_list[index_count] = ""
-            index_count += 1
-            return "".join(my_string_list)
+    string_copy = []
+    for char in my_string:
+        if (char != 'c') and (char != 'C'):
+            string_copy.append(char)
+    return "".join(string_copy)
