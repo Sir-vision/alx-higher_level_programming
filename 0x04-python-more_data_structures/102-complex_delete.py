5296a62f6443a58911aab6a63077583df10b359b
Oyebode <adetunji.oyebode6@gmail.com>
2022-12-13 00:17:12 +0100

    task15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    while value in a dictionary.values():
+    while value in a_dictionary.values():
         for key, word in a_dictionary.items():
             if word == value:
                 del a_dictionary[key]
