5b40ce4cf47b34ef6f99626be55986ab24ccbb9f
Oyebode <adetunji.oyebode6@gmail.com>
2023-01-05 00:52:32 +0100

    task0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Define a function: add_integer"""
+
+
+def add_integer(a, b=98):
+    """Adds two integers
+    Args:
+        @a (int, float): operand 1
+        @b (int, float): operand 2
+
+    Raises:
+        TypeError: if either a or b is not an int or float
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError('a must be an integer')
+
+    if not isinstance(b, (int, float)):
+        raise TypeError('b must be an integer')
+
+    return int(a) + int(b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,4 @@
+# Python - Test-driven development
+## doctest — Test interactive Python examples
+## doctest – Testing through documentation
+## Unit Tests in Python
