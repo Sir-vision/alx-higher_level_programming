1762c89604c7650e710f7a78478af7f0c35b6d7a
Oyebode <adetunji.oyebode6@gmail.com>
2023-01-06 00:54:22 +0100

    task5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Define a function text_indentation"""
+
+
+def text_indentation(text):
+    """prints a text with 2 new lines after each of these characters
+    Args:
+        ., ? and :
+    """
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+    i = 0
+    while i < len(text):
+        print(text[i], end='')
+        if text[i] in ['.', '?', ':']:
+            print()
+            print()
+            i += 1
+        i += 1
