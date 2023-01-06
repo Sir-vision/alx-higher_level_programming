61b33d8c070c7556509e2c3fd039d8243e6b96a9
Oyebode <adetunji.oyebode6@gmail.com>
2023-01-06 01:28:47 +0100

    task5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -11,10 +11,9 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError('text must be a string')
     i = 0
-    while i < len(text):
+    while (i < len(text)):
         print(text[i], end='')
         if text[i] in ['.', '?', ':']:
-            print()
-            print()
+            print('\n')
             i += 1
         i += 1
